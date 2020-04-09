6344168104254326aeb92400d4e68c04d30e43f8
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-09 17:19:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ function mondayWork(activity = 'go to the office') {
 
 mondayWork()
 
-function wrapAdjective(adj, flair = '*') {
+function wrapAdjective(adj, flair) {
   return function() { 
     return `You are ${flair}${adj}${flair}!`
   }
 }
 
-wrapAdjective('a hard worker')
+wrapAdjective('a hard worker', '*')
