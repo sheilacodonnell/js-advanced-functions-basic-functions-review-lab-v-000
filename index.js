db80f6e3b21bc745478189c6016f5a743e6f85f8
Sheila O'Donnell <sheila@contexttravel.com>
2020-04-09 17:57:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function actionApplyer (startingPoint, functionArray) {
   let start = startingPoint;
   // for (let i = 0; i < functionArray.length; i++) {
   //   start = (functionArray[i])(start);
-  start.forEach(function(){console.log('hi')});
+  start.forEach(function(){});
   }
   return start;
 }
